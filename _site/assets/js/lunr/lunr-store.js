4bbdc1c5db1f84bdc95ad86313903b2981e5c877
Shakarr <risaaflo@gmail.com>
2021-05-25 01:29:55 +0200

    segundo post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -3,4 +3,9 @@ var store = [{
         "excerpt":"Entender como funciona la red es algo muy importante para cualquier informático que se precie, pues bien aquí aprenderas los conocimientos básicos sobre el tema. El modelo OSI El modelo OSI (Open Systems Interconnection), traducido del ingles, sistemas abiertos de interconección, es un modelo estandarizado que se usa para demostrar...","categories": ["Explicaciones"],
         "tags": ["Tutoriales","Red","OSI","Capa","Internet"],
         "url": "http://localhost:4000/explicando-el-modelo-osi/",
-        "teaser":"http://localhost:4000/assets/images/explicacion-del-modelo-osi/banner-osi.png"}]
+        "teaser":"http://localhost:4000/assets/images/explicacion-del-modelo-osi/banner-osi.png"},{
+        "title": "Conectarse a otro pc por tunelación SSH",
+        "excerpt":"Porque voy a querer conectarme por tunelación ssh a otro PC? Pues bien quedate y verás el porque, pero antes como siempre un poco de teoría para entender el concepto. SH o Secure Shell es un protocolo el cual facilita las comunicaciones “seguras” entre dos sistemas usando una arquitectura del...","categories": ["Explicaciones"],
+        "tags": ["Tutoriales","Túneles","SSH"],
+        "url": "http://localhost:4000/tunel-ssh/",
+        "teaser":"http://localhost:4000/assets/images/tunel-ssh/ssh-tunneling.png"}]
