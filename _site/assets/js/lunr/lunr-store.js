cc10fa4badca5d5d458b9f997e7344cc22f7f8d0
shakar <shakar@localhost.localdomain>
2021-08-20 18:26:48 +0100

    cuarto post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -9,8 +9,13 @@ var store = [{
         "tags": ["Tutoriales","Túneles","SSH"],
         "url": "http://localhost:4000/tunel-ssh/",
         "teaser":"http://localhost:4000/assets/images/tunel-ssh/ssh-tunneling.png"},{
-        "title": "Creando 1000+ hostposts",
-        "excerpt":"En este post vamos a tratar uno de los muchos metodos de como crear un hostpost, para esta prueba utilizaremos mdk3 y lo que haremos será un ataque BEACOON FLOOD donde lo que tendrá por objetivo es crear un montón de puntos de acceso con el mismo nombre ESSID que...","categories": ["Explicaciones"],
+        "title": "Creando 1000+ hotsposts",
+        "excerpt":"En este post vamos a tratar uno de los muchos metodos de como crear un hotspost, para esta prueba utilizaremos mdk3 y lo que haremos será un ataque BEACOON FLOOD donde lo que tendrá por objetivo es crear un montón de puntos de acceso con el mismo nombre ESSID que...","categories": ["Explicaciones"],
         "tags": ["Tutoriales","Redes","Ataques","mdk3","hacking"],
         "url": "http://localhost:4000/creando-1000-hostpost/",
-        "teaser":"http://localhost:4000/assets/images/creando-1000-hostpost/banner.webp"}]
+        "teaser":"http://localhost:4000/assets/images/creando-1000-hostpost/banner.webp"},{
+        "title": "Como esconder un backdoor en un documento",
+        "excerpt":"En este post vamos a tratar un sencillo metodo de intrusión el cual consiste en esconde un backdoor en un documento, como puede ser word, pdf,…etc. Primero vamos a necesitar una herramienta externa desde los repositorios de github, la cual es ps1encode: https://github.com/CroweCybersecurity/ps1encode. git clone https://github.com/CroweCybersecurity/ps1encode.git Al ejecutarlo vemos que...","categories": ["Intrusion"],
+        "tags": ["Tutoriales","Backdoor","Ataques","hacking"],
+        "url": "http://localhost:4000/esconder-backdoor-en-documento/",
+        "teaser":"http://localhost:4000/assets/images/esconder-backdoor-en-documento/banner-backdoor-intrusion.png"}]
