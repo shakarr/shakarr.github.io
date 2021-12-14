0d8b9838bd4d49228e33da316e8f9508f573f214
shakarr <risaaflo@gmail.com>
2021-12-14 16:25:07 +0000

    quinto post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -18,4 +18,9 @@ var store = [{
         "excerpt":"En este post vamos a tratar un sencillo metodo de intrusión el cual consiste en esconde un backdoor en un documento, como puede ser word, pdf,…etc. Primero vamos a necesitar una herramienta externa desde los repositorios de github, la cual es ps1encode: https://github.com/CroweCybersecurity/ps1encode. git clone https://github.com/CroweCybersecurity/ps1encode.git Al ejecutarlo vemos que...","categories": ["Intrusion"],
         "tags": ["Tutoriales","Backdoor","Ataques","hacking"],
         "url": "http://localhost:4000/esconder-backdoor-en-documento/",
-        "teaser":"http://localhost:4000/assets/images/esconder-backdoor-en-documento/banner-backdoor-intrusion.png"}]
+        "teaser":"http://localhost:4000/assets/images/esconder-backdoor-en-documento/banner-backdoor-intrusion.png"},{
+        "title": "Beep HTB Writeup",
+        "excerpt":"1. Enumeration Vamos a empezar como siempre con la enumeración de puertos, vamos a usar el siguiente comando: nmap -sC -sV -Pn -oA nmap/result 10.10.10.7. Vamos a navegar a la url ya que vemos que el peurto 80 esta abierto, pero si navegamos a la url no saldrá algo como...","categories": ["HTB Writeup"],
+        "tags": ["Tutoriales","HTB","Machine","hacking"],
+        "url": "http://localhost:4000/beep-writeup-htb/",
+        "teaser":"http://localhost:4000/assets/images/beep-writeup-htb/beep-banner.png"}]
