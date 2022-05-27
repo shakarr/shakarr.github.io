830fb71d56a7b3650f60412459ad27902bf8cf6a
shakarr <risaaflo@gmail.com>
2022-05-27 19:30:15 +0200

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ApplicationComponent } from './application/application.component';
 import { WaifuComponent } from './waifu/waifu.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SocialComponent } from './social/social.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { TrainingComponent } from './training/training.component';
+import { AchievementComponent } from './achievement/achievement.component';
+import { HomeComponent } from './home/home.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { QuoteComponent } from './quote/quote.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ApplicationComponent,
-    WaifuComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/application/application.component.ts
+++ src/app/application/application.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { loadExternalResource } from 'src/loadExternalResource';
+
+declare var WOW : any;
 
 @Component({
   selector: 'app-application',
   templateUrl: './application.component.html',
-  styleUrls: ['./application.component.css']
+  styleUrls: ['./application.component.css'],
 })
 export class ApplicationComponent implements OnInit {
+  public checkScreenSize: boolean = screen.width >= 768;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    if (this.checkScreenSize) {
+      loadExternalResource('assets/js/wow.min.js')
+        .then(() => {
+          let wow = new WOW({
+            boxClass: 'wow',
+            animateClass: 'animated',
+            offset: 0,
+          });
+          wow.init();
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
   }
-
 }
